4155e22e5240ee1cec3a8aa7344d0edb4d26c863
elguapo87 <pglogovac@gmail.com>
2025-06-26 18:07:13 +0200

    Created RightSidebar component

--- src/components/ChatContainer.tsx
+++ src/components/ChatContainer.tsx
@@ -16,9 +16,11 @@ type UserType = {
 type HomePageProps = {
   selectedUser: UserType | null;
   setSelectedUser: React.Dispatch<React.SetStateAction<UserType | null>>;
+  showRightSide?: boolean;
+  setShowRightSide?: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
-const ChatContainer = ({ selectedUser, setSelectedUser }: HomePageProps) => {
+const ChatContainer = ({ selectedUser, setSelectedUser, showRightSide, setShowRightSide }: HomePageProps) => {
 
   const scrollEnd = useRef<HTMLDivElement | null>(null);
 
@@ -28,7 +30,7 @@ const ChatContainer = ({ selectedUser, setSelectedUser }: HomePageProps) => {
     }
   }, []);
 
-  return selectedUser ? (
+  return selectedUser && !showRightSide ? (
     <div className="h-full overflow-scroll relative backdrop-blur-lg">
       {/* HEADER */}
       <div className="flex items-center gap-3 py-3 mx-4 border-b border-stone-500">
@@ -38,9 +40,9 @@ const ChatContainer = ({ selectedUser, setSelectedUser }: HomePageProps) => {
           John Doe
           <span className="w-2 h-2 rounded-full bg-green-500"></span>
         </p>
-
-        <Image onClick={() => { setSelectedUser(null) }} src={assets.arrow_icon} alt="" className="md:hidden max-w-7" />
         
+        <Image onClick={() => { setSelectedUser(null) }} src={assets.arrow_icon} alt="" className="md:hidden max-w-7" />
+        <Image onClick={() => setShowRightSide?.(true)} src={assets.arrow_icon} alt="" className="md:hidden rotate-180 max-w-7" />
         
       </div>
 
