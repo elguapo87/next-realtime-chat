e4e2f95975c74f9862c2c6dc170020443eed7b13
elguapo87 <pglogovac@gmail.com>
2025-06-27 14:45:58 +0200

    Created profilePage

--- src/app/profilePage/page.tsx
+++ src/app/profilePage/page.tsx
@@ -1,7 +1,42 @@
+"use client"
+
+import Image from "next/image"
+import { useRouter } from "next/navigation";
+import { useState } from "react"
+import assets from "../../../public/assets";
+
 const ProfilePage = () => {
+
+  const [selectedImg, setSelectedImg] = useState<File | null>(null);
+  const [name, setName] = useState("John Doe");
+  const [bio, setBio] = useState("Hi everyone, I am Using RealChat");
+
+  const router = useRouter();
+
+  const handleSubmit = async (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+    router.push("/");
+  };
+
   return (
-    <div>
-      ProfilePage
+    <div className="min-h-screen bg-cover bg-no-repeat flex items-center justify-center">
+      <div className="w-5/6 max-w-2xl backdrop-blur-2xl text-gray-300 border-2 border-gray-600 flex items-center justify-between max-sm:flex-col-reverse rounded-lg">
+        <form onSubmit={handleSubmit} className="flex flex-col gap-5 p-10 flex-1">
+          <h3 className="text-lg">Profile Details</h3>
+          <label htmlFor="avatar" className="flex items-center gap-3 cursor-pointer">
+            <input onChange={(e) => setSelectedImg(e.target.files && e.target.files[0])} type="file" id="avatar" accept=".png, .jpg, .jpeg" hidden />
+            <Image src={selectedImg ? URL.createObjectURL(selectedImg) : assets.avatar_icon} width={100} height={100} alt="" className={`w-12 h-12 ${selectedImg && "rounded-full"}`} />
+            Upload profile image
+          </label>
+
+          <input onChange={(e) => setName(e.target.value)} value={name} type="text" placeholder="Your name" className="p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-violet-500" required />
+          <textarea onChange={(e) => setBio(e.target.value)} value={bio} rows={4} placeholder="Write profile bio" className="p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-violet-500" required></textarea>
+
+          <button type="submit" className="bg-gradient-to-r from-purple-400 to-violet-600 text-white p-2 rounded-full text-lg cursor-pointer">Save</button>
+        </form>
+
+        <Image src={assets.logo_icon} alt="" className="max-w-44 aspect-square rounded-full mx-10 max-sm:mt-10" />
+      </div>
     </div>
   )
 }
